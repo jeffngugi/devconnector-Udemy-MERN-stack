f3eb32c708fcf0d679e00ca184655b03576e0299
jeffngugi <ngugimwathi254@gmail.com>
2019-09-01 10:21:55 +0300

    Like and unlike posts

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -87,4 +87,66 @@ router.delete(
   }
 );
 
+//@route POST api/posts/like/:id
+//@desc Like post
+//@access Private
+router.post(
+  '/like/:id',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    Profile.findOne({ user: req.user.id }).then((profile) => {
+      Post.findById(req.params.id)
+        .then((post) => {
+          if (
+            post.likes.filter((like) => like.user.toString() === req.user.id)
+              .length > 0
+          ) {
+            return res
+              .status(400)
+              .json({ alreadyliked: 'User already liked this post' });
+          }
+          //Add user id to likes array
+          post.likes.unshift({ user: req.user.id });
+          post.save().then((post) => res.json(post));
+        })
+        .catch((err) =>
+          res.status(404).json({ postnotfound: 'Post not found' })
+        );
+    });
+  }
+);
+
+//@route POST api/posts/unlike/:id
+//@desc Unlike post
+//@access Private
+router.post(
+  '/unlike/:id',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    Profile.findOne({ user: req.user.id }).then((profile) => {
+      Post.findById(req.params.id)
+        .then((post) => {
+          if (
+            post.likes.filter((like) => like.user.toString() === req.user.id)
+              .length === 0
+          ) {
+            return res
+              .status(400)
+              .json({ notliked: 'You have not like the post' });
+          }
+          //Get remove index
+          const removeIndex = post.likes
+            .map((item) => item.user.toString())
+            .indexOf(req.user.id);
+          //splice  out of array
+          post.likes.splice(removeIndex, 1);
+          post.save().then((post) => res.json(post));
+        })
+        .catch((err) =>
+          res.status(404).json({ postnotfound: 'Post not found' })
+        );
+    });
+  }
+);
+
 module.exports = router;

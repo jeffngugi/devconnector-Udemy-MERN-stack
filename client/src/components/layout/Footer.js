d4e56cb58a3b7de0a327216a79e7b32e01d8b72b
jeffngugi <ngugimwathi254@gmail.com>
2019-09-02 10:29:38 +0300

    Added and set up client side of the project

---
+++ client/src/components/layout/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer class='bg-dark text-white mt-5 p-4 text-center'>
+      Copyright &copy; {new Date().getFullYear()} Jeff Ngugi
+    </footer>
+  );
+};
+
+export default Footer;

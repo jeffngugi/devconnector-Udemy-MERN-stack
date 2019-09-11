4a9f93886e6360b475ff46eaeccf0f5cbc03ed0a
jeffngugi <ngugimwathi254@gmail.com>
2019-09-11 22:23:07 +0300

    Keys

--- config/keys_prod.js
+++ config/keys_prod.js
@@ -1,4 +1,5 @@
 module.exports = {
-  mongoURI: process.env.mongoURI,
-  secretOrKey: process.env.SECRET_OR_KEY
+  mongoURI:
+    'mongodb+srv://devcon1:devcon1@cluster0-vogbf.mongodb.net/test?retryWrites=true&w=majority',
+  secretOrKey: 'secret'
 };
